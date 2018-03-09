3e60c4229d6aec2f47b3832bb7cd7962d2c03e14
Beeerrt <b.geerken@web.de>
2018-03-09 14:39:00 +0100

    Change

--- controller/infounitController.js
+++ controller/infounitController.js
@@ -4,32 +4,6 @@ var infounitModel = mongoose.model('infounit');
 var _ = require('underscore');
 
 exports.post = function(req, res){
-    //var infounit = mongoose.model('infounit',infounitModile);
-    // var unit = new infounit ({
-    //     nodeid: 1,      
-    //     temperatura: '12',
-    //     incline: '12',
-    //     humidity: '12',
-    //     brightness: '12',
-    //     positon: 'wohnzimmer'
-    //   });
-    
-    // var newInfounit = new infounitModel(unit);
-    
-    // var unit = new infounit ({
-    //     nodeid: 1,      
-    //     temperatura: '12',
-    //     incline: '12',
-    //     humidity: '12',
-    //     brightness: '12',
-    //     positon: 'wohnzimmer'
-    //   });
-    //   unit.save(function(err){
-    //     if(err)
-    //     {
-    //         console.log(err);
-    //     }
-    //   });
       
     var newinfounit = new infounitModel(req.body);
     newinfounit.save();
