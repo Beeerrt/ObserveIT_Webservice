60aa05faa3ac38a4d83e8877b80bd26b70311984
Beeerrt <b.geerken@web.de>
2018-04-02 01:53:57 +0200

    Kommentar gelöscht

--- models/limitModel.js
+++ models/limitModel.js
@@ -2,15 +2,11 @@ var mongoose = require('mongoose');
 var schmea = mongoose.Schema;
 
 var limitSchema = new schmea({
-    //neigung
     maxTemperatur: String,
     minTemperatur: String,
-    //neigungs 
     avgIncline: String,
-    //Luftfeuchtigkeit
     maxHumidity: String,
     minHumidity: String,
-     //helligkeit
     maxBrightness: String,
     minBrightness: String
 });
